bb216916b84367f5d894d9d7038c8f1ece2c60fe
tiritto <github@niedzwiedzki.tech>
2020-01-27 01:24:09 +0100

    Supplied options are now merged with default options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PluginError = require('plugin-error');
 const through = require('through2');
 const crass = require('crass');
+
 /**
  * Creates a PluginError with the provided message
  *
@@ -9,19 +10,7 @@ const crass = require('crass');
  */
 const withError = details => new PluginError('gulp-crass-minifier', details);
 
-module.exports = (options = {
-    optimize: true,
-    pretty: false,
-    css4: false,
-    o1: true,
-    saveie: false,
-    browser_min: {
-        chrome: 39,
-        firefox: 31,
-        ie: 11,
-        opera: 26,
-    }
-}) => through.obj(function (file, encoding, done) {
+module.exports = (options = {}) => through.obj(function (file, encoding, done) {
 
     // Ignore empty files
     if (file === null) return done(null, file);
@@ -31,7 +20,20 @@ module.exports = (options = {
         return done(withError('Streaming is not supported. You might want to use gulp-buffer or vinyl-buffer to get around this issue.'));
     }
 
-    // TODO: Merge default configuration object and supplied configuration object to apply defaults to missing fields
+    // Merge options with default settings
+    options = Object.assign({
+        optimize: true,
+        pretty: false,
+        css4: false,
+        o1: true,
+        saveie: false,
+        browser_min: {
+            chrome: 39,
+            firefox: 31,
+            ie: 11,
+            opera: 26,
+        }
+    }, options);
 
     // Minify CSS using Crass
     try {
